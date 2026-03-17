9cacfe96404b761da417eea62c088679abb68afc
OviO17 <oviea4@gmail.com>
2026-03-17 23:09:37 +0000

    Add interactive terminal (node + socket)

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const http = require("http");
-const { spawn } = require("child_process");
+const express = require('express');
+const app = express();
+const path = require('path');
+const { spawn } = require('child_process');
 
-const server = http.createServer((req, res) => {
-  const process = spawn("python", ["run.py"]);
+app.use(express.static('views'));
 
-  process.stdout.on("data", (data) => {
-    res.write(data);
-  });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
 
-  process.stderr.on("data", (data) => {
-    res.write(data);
-  });
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-  process.on("close", () => {
-    res.end();
-  });
+io.on('connection', (socket) => {
+    const python = spawn('python3', ['run.py']);
+
+    python.stdout.on('data', (data) => {
+        socket.emit('output', data.toString());
+    });
+
+    socket.on('input', (data) => {
+        python.stdin.write(data + '\n');
+    });
 });
 
 server.listen(process.env.PORT || 3000);
